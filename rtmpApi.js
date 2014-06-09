92331e9cbf8c230f818ec5e28515114146cd0593
Delian Delchev <delian.delchev@gmail.com>
2014-06-09 14:09:40 +0000

    Small fixed of the dependencies
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- rtmpApi.js
+++ rtmpApi.js
@@ -2,7 +2,7 @@ module.exports = {
    log: require('./log.js'),
    rtmpServer: require('./rtmpServer.js'),
    rtmpSession: require('./rtmpSession.js'),
-   rtmpQueue: require('./rtmpQueue.js'),
+   rtmpQueue: require('./readQueue.js'),
    rtmpChunk: require('./rtmpChunk.js'),
    rtmpMessage: require('./rtmpMessage.js')
 }
